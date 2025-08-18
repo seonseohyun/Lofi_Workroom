bbbb338fcc56a009e26eb2859a93cbb47b582972
seonseo <seonseo1216@gmail.com>
2025-08-18 23:43:57 +0900

    todoWidget_final_ver

---
+++ lofi/ViewModels/MusicPlayerViewModel.cs
@@ -0,0 +1,65 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace lofi.ViewModels
+{
+    public class MusicPlayerViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<string> Tracks { get; } = new();
+
+        private string? _currentTrack;
+        public string? CurrentTrack
+        {
+            get => _currentTrack;
+            set { _currentTrack = value; OnPropertyChanged(); }
+        }
+
+        private bool _isPlaying;
+        public bool IsPlaying
+        {
+            get => _isPlaying;
+            set { _isPlaying = value; OnPropertyChanged(); OnPropertyChanged(nameof(PlayPauseLabel)); }
+        }
+
+        public string PlayPauseLabel => IsPlaying ? "⏸" : "▶";
+
+        public ICommand PlayPauseCommand { get; }
+        public ICommand NextCommand { get; }
+        public ICommand PrevCommand { get; }
+
+        public MusicPlayerViewModel()
+        {
+            // 예시: 초기 트랙 목록 (추후 파일 로딩 로직 연결)
+            Tracks.Add("Track 1");
+            Tracks.Add("Track 2");
+            Tracks.Add("Track 3");
+
+            //PlayPauseCommand = new RelayCommand(_ => TogglePlayPause());
+            //NextCommand = new RelayCommand(_ => NextTrack());
+            //PrevCommand = new RelayCommand(_ => PrevTrack());
+
+            CurrentTrack = Tracks.Count > 0 ? Tracks[0] : null;
+        }
+
+        private void TogglePlayPause() => IsPlaying = !IsPlaying;
+        private void NextTrack()
+        {
+            if (Tracks.Count == 0) return;
+            var i = Tracks.IndexOf(CurrentTrack!);
+            CurrentTrack = Tracks[(i + 1) % Tracks.Count];
+        }
+        private void PrevTrack()
+        {
+            if (Tracks.Count == 0) return;
+            var i = Tracks.IndexOf(CurrentTrack!);
+            CurrentTrack = Tracks[(i - 1 + Tracks.Count) % Tracks.Count];
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string? n = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(n));
+    }
+}
---
+++ lofi/Views/MusicPlayer.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lofi.Views
+{
+    public partial class MusicPlayer : UserControl
+{
+    private const double MinW = 260;
+    private const double MinH = 180;
+
+    public MusicPlayer()
+    {
+        InitializeComponent();
+    }
+
+    // 부모 Canvas 찾기
+    private Canvas? HostCanvas()
+    {
+        DependencyObject d = this;
+        while (d != null && d is not Canvas)
+            d = VisualTreeHelper.GetParent(d);
+        return d as Canvas;
+    }
+
+    // === 변 리사이즈 ===
+    private void ResizeLeft_OnDragDelta(object sender, DragDeltaEventArgs e)
+    {
+        double newW = System.Math.Max(MinW, ActualWidth - e.HorizontalChange);
+        double dx = ActualWidth - newW;
+        Width = newW;
+
+        var canvas = HostCanvas();
+        if (canvas != null)
+        {
+            double left = Canvas.GetLeft(this);
+            if (double.IsNaN(left)) left = 0;
+            Canvas.SetLeft(this, left + dx);
+        }
+        e.Handled = true;
+    }
+
+    private void ResizeRight_OnDragDelta(object sender, DragDeltaEventArgs e)
+    {
+        Width = System.Math.Max(MinW, ActualWidth + e.HorizontalChange);
+        e.Handled = true;
+    }
+
+    private void ResizeTop_OnDragDelta(object sender, DragDeltaEventArgs e)
+    {
+        double newH = System.Math.Max(MinH, ActualHeight - e.VerticalChange);
+        double dy = ActualHeight - newH;
+        Height = newH;
+
+        var canvas = HostCanvas();
+        if (canvas != null)
+        {
+            double top = Canvas.GetTop(this);
+            if (double.IsNaN(top)) top = 0;
+            Canvas.SetTop(this, top + dy);
+        }
+        e.Handled = true;
+    }
+
+    private void ResizeBottom_OnDragDelta(object sender, DragDeltaEventArgs e)
+    {
+        Height = System.Math.Max(MinH, ActualHeight + e.VerticalChange);
+        e.Handled = true;
+    }
+
+    // === 코너 리사이즈(조합) ===
+    private void ResizeTopLeft_OnDragDelta(object sender, DragDeltaEventArgs e)
+    {
+        ResizeLeft_OnDragDelta(sender, e);
+        ResizeTop_OnDragDelta(sender, e);
+    }
+
+    private void ResizeTopRight_OnDragDelta(object sender, DragDeltaEventArgs e)
+    {
+        ResizeRight_OnDragDelta(sender, e);
+        ResizeTop_OnDragDelta(sender, e);
+    }
+
+    private void ResizeBottomLeft_OnDragDelta(object sender, DragDeltaEventArgs e)
+    {
+        ResizeLeft_OnDragDelta(sender, e);
+        ResizeBottom_OnDragDelta(sender, e);
+    }
+
+    private void ResizeBottomRight_OnDragDelta(object sender, DragDeltaEventArgs e)
+    {
+        ResizeRight_OnDragDelta(sender, e);
+        ResizeBottom_OnDragDelta(sender, e);
+    }
+}
+}
